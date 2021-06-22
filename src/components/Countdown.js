05c29841e8b5a7af1007180bcc3be17669599d14
Elena Lushnikova <elushnikova.256@gmail.com>
2021-06-22 10:27:41 +0300

    style(Countdown): Rename count to ms

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -2,23 +2,23 @@ import { useEffect, useState } from "react";
 
 function Countdown() {
   const sec = 1000;
-  const [count, setCount] = useState(60 * sec);
+  const [ms, setMs] = useState(60 * sec);
   const minusSecond = (prev) => prev - sec;
 
   useEffect(() => {
-    if (!count) return;
-    const id = setTimeout(() => setCount(minusSecond), sec);
+    if (!ms) return;
+    const id = setTimeout(() => setMs(minusSecond), sec);
     return () => clearTimeout(id);
-  }, [count]);
+  }, [ms]);
 
   return (
     <section>
       <h2>Countdown</h2>
-      <p>{count ? `${count / 1000} s` : "Time is up"}</p>
+      <p>{ms ? `${ms / 1000} s` : "Time is up"}</p>
 
-      <button onClick={() => setCount(5 * sec)}>5 seconds</button>
-      <button onClick={() => setCount(60 * sec)}>1 minute</button>
-      <button onClick={() => setCount(5 * 60 * sec)}>5 minutes</button>
+      <button onClick={() => setMs(5 * sec)}>5 seconds</button>
+      <button onClick={() => setMs(60 * sec)}>1 minute</button>
+      <button onClick={() => setMs(5 * 60 * sec)}>5 minutes</button>
     </section>
   );
 }
