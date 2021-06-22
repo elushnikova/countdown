e73ac355961ada2ec97d0dd785e4e605a6ccc49d
Elena Lushnikova <elushnikova.256@gmail.com>
2021-06-22 17:10:44 +0300

    fix: Close Settings on click outside

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,47 +1,54 @@
+import { useContext } from "react";
 import { animated } from "react-spring";
+import TimerContext from "../../contexts/TimerContext";
 import SettingsButton from "../SettingsButton/SettingsButton";
 import preset from "../Timer/utils/preset";
 import classes from "./Settings.module.scss";
 
 function Settings({ style, isInverted }) {
+  const { setOpen } = useContext(TimerContext);
+
   return (
-    <animated.div
-      className={`${classes.block} ${isInverted && classes.inverted}`}
-      style={style}
-    >
-      <ul className={classes.list}>
-        <li>
-          <SettingsButton isInverted={isInverted} time={preset.fiveSec}>
-            5 сек
-          </SettingsButton>
-        </li>
-        <li>
-          <SettingsButton isInverted={isInverted} time={preset.tenSec}>
-            10 сек
-          </SettingsButton>
-        </li>
-        <li>
-          <SettingsButton isInverted={isInverted} time={preset.oneMin}>
-            1 мин
-          </SettingsButton>
-        </li>
-        <li>
-          <SettingsButton isInverted={isInverted} time={preset.fiveMin}>
-            5 мин
-          </SettingsButton>
-        </li>
-        <li>
-          <SettingsButton isInverted={isInverted} time={preset.tenMin}>
-            10 мин
-          </SettingsButton>
-        </li>
-        <li>
-          <SettingsButton isInverted={isInverted} time={preset.fifteenMin}>
-            15 мин
-          </SettingsButton>
-        </li>
-      </ul>
-    </animated.div>
+    <div className={classes.overlay} onClick={() => setOpen(false)}>
+      <animated.div
+        className={`${classes.block} ${isInverted && classes.inverted}`}
+        style={style}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <ul className={classes.list}>
+          <li>
+            <SettingsButton isInverted={isInverted} time={preset.fiveSec}>
+              5 сек
+            </SettingsButton>
+          </li>
+          <li>
+            <SettingsButton isInverted={isInverted} time={preset.tenSec}>
+              10 сек
+            </SettingsButton>
+          </li>
+          <li>
+            <SettingsButton isInverted={isInverted} time={preset.oneMin}>
+              1 мин
+            </SettingsButton>
+          </li>
+          <li>
+            <SettingsButton isInverted={isInverted} time={preset.fiveMin}>
+              5 мин
+            </SettingsButton>
+          </li>
+          <li>
+            <SettingsButton isInverted={isInverted} time={preset.tenMin}>
+              10 мин
+            </SettingsButton>
+          </li>
+          <li>
+            <SettingsButton isInverted={isInverted} time={preset.fifteenMin}>
+              15 мин
+            </SettingsButton>
+          </li>
+        </ul>
+      </animated.div>
+    </div>
   );
 }
 
