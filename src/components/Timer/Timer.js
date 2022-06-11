4cf0edb0faa90ee0c40813d485f11f7de7f0d9d4
Elena Lushnikova <elushnikova.256@gmail.com>
2022-06-11 11:18:54 +0300

    refactor: Separate timer effects to custom hooks

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,7 +1,5 @@
-import { useEffect } from 'react';
 import { second } from './utils/preset';
 import {
-  subtractSecond,
   padNumber,
   truncate,
   divideByMinute,
@@ -11,44 +9,14 @@ import {
 } from './utils/lib';
 import styles from './Timer.module.scss';
 import useTimerContext from '../../hooks/useTimerContext';
-import useConfigContext from '../../hooks/useConfigContext';
+import useGetQuerySecondsEffect from '../../hooks/useGetQuerySecondsEffect';
+import useStartTimerEffect from '../../hooks/useStartTimerEffect';
 
 function Timer() {
-  const { time, setTime } = useTimerContext();
-  const { maxMinutes, queryKeySeconds, replaceHistoryEntry } = useConfigContext();
+  const { time } = useTimerContext();
 
-  const acceptSeconds = () => {
-    const queryString = new URLSearchParams(document.location.search);
-    const querySeconds = queryString.get(queryKeySeconds);
-
-    if (!querySeconds) return;
-
-    let seconds = parseInt(querySeconds, 10);
-    if (seconds > maxMinutes * 60) {
-      seconds = maxMinutes * 60;
-    }
-
-    if (replaceHistoryEntry) {
-      window.history.replaceState(null, '', document.location.origin);
-    }
-
-    setTime(seconds * 1000);
-  };
-
-  const startTimer = () => {
-    if (!time) return undefined;
-
-    const id = setTimeout(() => {
-      setTime(subtractSecond);
-    }, second);
-
-    return () => {
-      clearTimeout(id);
-    };
-  };
-
-  useEffect(acceptSeconds, [setTime, queryKeySeconds, maxMinutes, replaceHistoryEntry]);
-  useEffect(startTimer, [time, setTime]);
+  useGetQuerySecondsEffect();
+  useStartTimerEffect();
 
   return (
     <>
