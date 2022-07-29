cecbe3b449cdfd5bee253d7f8db0ebe17a868f1b
Elena Lushnikova <elushnikova.256@gmail.com>
2022-07-29 21:18:51 +0300

    chore: Leave only 5 and 10 minute presets

--- src/components/Timer/utils/presets.js
+++ src/components/Timer/utils/presets.js
@@ -2,12 +2,8 @@ const second = 1000;
 const minute = 60 * second;
 
 const preset = {
-  fiveSec: 5 * second,
-  tenSec: 10 * second,
-  oneMin: minute,
   fiveMin: 5 * minute,
   tenMin: 10 * minute,
-  fifteenMin: 15 * minute,
 
   get default() { return this.tenMin; },
   get minimum() { return 1 * second; },
@@ -15,12 +11,8 @@ const preset = {
 };
 
 const presets = [
-  { title: '5 сек', duration: preset.fiveSec },
-  { title: '10 сек', duration: preset.tenSec },
-  { title: '1 мин', duration: preset.oneMin },
   { title: '5 мин', duration: preset.fiveMin },
   { title: '10 мин', duration: preset.tenMin },
-  { title: '15 мин', duration: preset.fifteenMin },
 ];
 
 export default presets;
